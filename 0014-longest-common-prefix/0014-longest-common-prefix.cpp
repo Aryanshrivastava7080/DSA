f51f5a6d1d43435b01b55ca3f7d8d4f3dcd573e2
Aryan Srivastava <145749664+Aryanshrivastava7080@users.noreply.github.com>
2026-06-10 21:15:49 +0530

    Time: 0 ms (100%), Space: 11.6 MB (99.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string prefix = strs[0];
+//         string prefix = strs[0];
 
-        for(int i = 1; i < strs.size(); i++) {
-            while(strs[i].find(prefix) != 0) {
-                prefix.pop_back();
+//         for(int i = 1; i < strs.size(); i++) {
+//             while(strs[i].find(prefix) != 0) {
+//                 prefix.pop_back();
+//             }
+//         }
+
+//         return prefix;
+//     }
+// };
+
+        string ans = "";
+
+        for (int i = 0; i < strs[0].size(); i++) {
+            char ch = strs[0][i];
+
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].size() || strs[j][i] != ch) {
+                    return ans;
+                }
             }
+
+            ans += ch;
         }
 
-        return prefix;
+        return ans;
     }
 };
