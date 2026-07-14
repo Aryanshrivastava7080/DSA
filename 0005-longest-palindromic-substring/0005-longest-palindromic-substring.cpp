c5d8a6b17b99c506e345451c436d7420d3c55b59
Aryan Srivastava <145749664+Aryanshrivastava7080@users.noreply.github.com>
2026-07-14 16:33:18 +0530

    Time: 10 ms (63.22%), Space: 9.3 MB (92.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,42 +2,77 @@ class Solution {
 public:
       
     string longestPalindrome(string s) {
-        int start = 0;
-        int maxLen = 1;
+        int start=0;
+        int maxlen=1;
+        int n= s.length();
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i=0; i<n; i++){
+            
+            // odd condition
+            int left=i;
+            int rght=i;
 
-            // Odd length palindrome
-            int left = i;
-            int right = i;
-
-            while (left >= 0 && right < s.length() && s[left] == s[right]) {
-
-                if (right - left + 1 > maxLen) {
-                    maxLen = right - left + 1;
-                    start = left;
+            while(left>=0 && rght<n && s[left]==s[rght]){
+                if(rght-left+1>maxlen){
+                    maxlen=rght-left+1;
+                    start=left;
                 }
-
                 left--;
-                right++;
+                rght++;
             }
 
-            // Even length palindrome
-            left = i;
-            right = i + 1;
-
-            while (left >= 0 && right < s.length() && s[left] == s[right]) {
-
-                if (right - left + 1 > maxLen) {
-                    maxLen = right - left + 1;
-                    start = left;
+            // even
+             left=i;
+             rght=i+1;
+            while(left>=0 && rght<n && s[left]==s[rght]){
+                if(rght-left+1>maxlen){
+                    maxlen=rght-left+1;
+                    start=left;
                 }
-
                 left--;
-                right++;
+                rght++;
             }
         }
+       return s.substr(start,maxlen);
 
-        return s.substr(start, maxLen);
     }
 };
+//         int start = 0;
+//         int maxLen = 1;
+
+//         for (int i = 0; i < s.length(); i++) {
+
+//             // Odd length palindrome
+//             int left = i;
+//             int right = i;
+
+//             while (left >= 0 && right < s.length() && s[left] == s[right]) {
+
+//                 if (right - left + 1 > maxLen) {
+//                     maxLen = right - left + 1;
+//                     start = left;
+//                 }
+
+//                 left--;
+//                 right++;
+//             }
+
+//             // Even length palindrome
+//             left = i;
+//             right = i + 1;
+
+//             while (left >= 0 && right < s.length() && s[left] == s[right]) {
+
+//                 if (right - left + 1 > maxLen) {
+//                     maxLen = right - left + 1;
+//                     start = left;
+//                 }
+
+//                 left--;
+//                 right++;
+//             }
+//         }
+
+//         return s.substr(start, maxLen);
+//     }
+// };
