4e400e1b05be03c35e891aca6c8f7c8ae7642507
Aryan Srivastava <145749664+Aryanshrivastava7080@users.noreply.github.com>
2026-07-21 16:16:33 +0530

    Time: 7 ms (85.38%), Space: 9.2 MB (92.09%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,41 +2,79 @@ class Solution {
 public:
       
     string longestPalindrome(string s) {
-        int start=0;
-        int maxlen=1;
-        int n= s.length();
+           int start=0;
+           int maxlen=1;
+           int n=s.length();
 
         for(int i=0; i<n; i++){
-            
-            // odd condition
+            //odd
             int left=i;
             int rght=i;
 
-            while(left>=0 && rght<n && s[left]==s[rght]){
-                if(rght-left+1>maxlen){
-                    maxlen=rght-left+1;
+             while(left>=0 && rght<n && s[left]==s[rght]){
+
+                if(rght-left+1 > maxlen){
+                    maxlen= rght-left+1;
                     start=left;
                 }
                 left--;
                 rght++;
             }
 
-            // even
+             //even
              left=i;
              rght=i+1;
+
             while(left>=0 && rght<n && s[left]==s[rght]){
-                if(rght-left+1>maxlen){
-                    maxlen=rght-left+1;
+                if(rght-left+1 > maxlen){
+                    maxlen= rght-left+1;
                     start=left;
                 }
                 left--;
                 rght++;
             }
+
         }
-       return s.substr(start,maxlen);
+
+           return s.substr(start,maxlen);
 
     }
 };
+//         int start=0;
+//         int maxlen=1;
+//         int n= s.length();
+
+//         for(int i=0; i<n; i++){
+            
+//             // odd condition
+//             int left=i;
+//             int rght=i;
+
+//             while(left>=0 && rght<n && s[left]==s[rght]){
+//                 if(rght-left+1>maxlen){   
+//                     maxlen=rght-left+1;
+//                     start=left;              //length updation
+//                 }
+//                 left--;
+//                 rght++;
+//             }
+
+//             // even
+//              left=i;
+//              rght=i+1;
+//             while(left>=0 && rght<n && s[left]==s[rght]){
+//                 if(rght-left+1>maxlen){
+//                     maxlen=rght-left+1;
+//                     start=left;
+//                 }
+//                 left--;
+//                 rght++;
+//             }
+//         }
+//        return s.substr(start,maxlen);
+
+//     }
+// };
 //         int start = 0;
 //         int maxLen = 1;
 
