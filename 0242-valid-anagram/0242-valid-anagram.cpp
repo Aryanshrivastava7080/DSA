9409b80fc664a2bfbd61315119a170f483d7fd4d
Aryan Srivastava <145749664+Aryanshrivastava7080@users.noreply.github.com>
2026-09-11 23:39:12 +0530

    Time: 0 ms (100%), Space: 9.6 MB (75.17%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        // Step 2: 26 lowercase English alphabets ke liye array
+        int count[26] = {0};
+
+        // Step 3: Frequency count karein
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++; // s ke character ki count badao
+            count[t[i] - 'a']--; // t ke character ki count ghatao
+        }
+
+        // Step 4: Check karein ki saare counts 0 hue ya nahi
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+};
